141b53828773ec06050061a371341c328fec29c1
JohnnyBoi <ninjakai25@gmail.com>
2023-04-04 12:46:46 -0500

    Add jokes array

---
+++ message.js
@@ -0,0 +1,28 @@
+const jokes = ["Dad, can you put my shoes on?", "No, I don't think they'll fit me.",
+"Why can't a nose be 12 inches long? Because then it would be a foot.",
+"What does a lemon say when it answers the phone? Yellow!",
+"This graveyard looks overcrowded. People must be dying to get in.",
+"What kind of car does an egg drive?A yolkswagen.",
+"Dad, can you put the cat out?I didn't know it was on fire.",
+"How do you make 7 even? Take away the s.",
+"How does a taco say grace? Lettuce pray.",
+"What time did the man go to the dentist? Tooth hurt-y.",
+"Why didn't the skeleton climb the mountain? It didn't have the guts.",
+"What do you call it when a snowman throws a tantrum? A meltdown.",
+"How many tickles does it take to make an octopus laugh? Ten tickles.",
+"I have a joke about chemistry, but I don't think it will get a reaction.",
+"What concert costs just 45 cents? 50 Cent featuring Nickelback!",
+"What does a bee use to brush its hair? A honeycomb!",
+"How do you make a tissue dance? You put a little boogie in it.",
+"Why did the math book look so sad? Because of all of its problems!",
+"What do you call cheese that isn't yours? Nacho cheese.",
+"My dad told me a joke about boxing. I guess I missed the punch line.",
+"What kind of shoes do ninjas wear? Sneakers!",
+"How does a penguin build its house? Igloos it together.",
+"How did Harry Potter get down the hill? Walking. JK! Rowling."];
+
+const le = jokes.length;
+
+console.log(le);
+console.log('nigga');
+console.log('why this shit not working');
